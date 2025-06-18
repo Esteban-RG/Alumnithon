1bc380750c0f149ed05bf7c3ebfe648e7c4e24d9
Esteban-RG <ricardoespace@gmail.com>
2025-06-18 02:24:38 -0600

    Se implemento Spring Security JWT

---
+++ src/main/java/com/sith/alumnithon/Auth/LoginRequest.java
@@ -0,0 +1,5 @@
+package com.sith.alumnithon.Auth;
+
+public record LoginRequest(String username, String password) {
+
+}
---
+++ src/main/java/com/sith/alumnithon/Auth/RegisterRequest.java
@@ -0,0 +1,5 @@
+package com.sith.alumnithon.Auth;
+
+public record RegisterRequest(String username, String password, String firstname, String lastname, String email) {
+
+}
